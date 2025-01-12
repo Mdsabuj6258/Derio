19428d6f473d371b44648eb65f581d8351f3cc9f
mdsabuj6258 <mdsabuj1925@gmail.com>
2025-01-12 18:31:25 +0600

    add 2 inner section

--- js/main.js
+++ js/main.js
@@ -1,23 +1,53 @@
 $(function () {
-    
-    "use strict";
-    
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    "use strict";
+
+
+    $('.select_2').select2({
+        placeholder: 'Select an option'
+    });
+
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+    $(".right_serch").on("click", function (event) {
+        $(".search_form_area").toggleClass("show_list");
+        event.stopPropagation();
+    });
+
+
+    // menu js
+    if ($(window).scrollTop() > 1) {
+        if ($('.main_menu').offset() != undefined) {
+            $('.main_menu').addClass('menu_fix');
+        }
+    } else {
+        if ($('.main_menu').offset() != undefined) {
+            $('.main_menu').removeClass('menu_fix');
+        }
+    }
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+
+            if ($('.main_menu').offset() != undefined) {
+                // check if menu_if class is already added
+                if (!$('.main_menu').hasClass("menu_fix")) {
+                    $('.main_menu').addClass("menu_fix");
+                }
+            }
+        }
+        else {
+            if ($('.main_menu').offset() != undefined) {
+                $('.main_menu').removeClass("menu_fix");
+            }
+        }
+    });
+
+
+
+
 
 });
